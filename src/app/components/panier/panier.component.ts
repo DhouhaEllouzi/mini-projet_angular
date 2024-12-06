90bb3bbf66a6beb259231b0c66d23f22d5ed53a3
s0ulaymachekir <129565073+s0ulaymachekir@users.noreply.github.com>
2024-12-06 01:05:49 +0100

    form done!

--- src/app/components/panier/panier.component.ts
+++ src/app/components/panier/panier.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../../services/products.service';
-import { Product } from '../../model/product.model';
 import { AppDataState, DataStateEnum } from '../../state/product.state';
 import { Observable, of } from 'rxjs';
 import { catchError, map, startWith } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { CurrencyPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../model/product.model';
 
 @Component({
   selector: 'app-panier',
   templateUrl: './panier.component.html',
   standalone: true,
-  imports: [CommonModule , FormsModule],
+  imports: [CommonModule, FormsModule],
 })
 export class PanierComponent implements OnInit {
   products$: Observable<AppDataState<Product[]>> | null = null;
   readonly DataStateEnum = DataStateEnum;
   selectedProducts: Product[] = [];
+
   constructor(private productsService: ProductsService, private router: Router) {}
 
   ngOnInit(): void {
@@ -28,16 +28,18 @@ export class PanierComponent implements OnInit {
   onGetSelectedProducts(): void {
     this.products$ = this.productsService.getSelectedProducts().pipe(
       map(data => {
-        console.log(data);
+        console.log('Fetched products:', data);
         this.selectedProducts = data || []; // Ensure selectedProducts is not null
         return { dataState: DataStateEnum.LOADED, data: data };
       }),
       startWith({ dataState: DataStateEnum.LOADING }),
-      catchError(err =>
-        of({ dataState: DataStateEnum.ERROR, errorMessage: err.message })
-      )
+      catchError(err => {
+        console.error('Error fetching selected products:', err);
+        return of({ dataState: DataStateEnum.ERROR, errorMessage: err.message });
+      })
     );
   }
-
-  
+  buy() {
+    this.router.navigate(['/form']);
+  }
 }
